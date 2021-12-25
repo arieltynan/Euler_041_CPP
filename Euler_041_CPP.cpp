9a5f48b1de6481657f5f1c94c5f5cbf50a59b825
Ariel Tynan <tynan.a@northeastern.edu>
2021-12-25 01:02:06 -0700

    Working solution, takes around 30 seconds

--- Euler_041_CPP.cpp
+++ Euler_041_CPP.cpp
@@ -3,14 +3,15 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <iterator>
 
 using namespace std;
 
 int main()
 {
-    vector<int> primes;
-    int total = 0; //total num of circular primes
-    int n = 100;//primes less than n (1000000)
+    vector<int> primes; //vector containing primes
+    int n = 10000000;
     primes.push_back(2);
     for (int i = 3; i < n; i++)
     {
@@ -28,7 +29,50 @@ int main()
             primes.push_back(i);
             //cout << i << " ";
         }
-    } // end for loop, prime search
-}
+    } //end for loop prime search
 
-int digits[9] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    for(int i = 0; i < primes.size(); i++)
+    {
+        int digits[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int temp = primes[i];
+        vector<int> vect;
+        bool exists = true;
+        while (temp != 0) //separates ints into digits
+        {
+            if (find(begin(digits), end(digits), temp % 10) != end(digits) == true && temp % 10 != 0)
+            { 
+                digits[(temp % 10) - 1] = 0; // removes digits that exist in int
+            }
+            else //if digit appears twice or is zero, prime is invalid
+            {
+                exists = false;
+            }
+            vect.push_back(temp % 10); //stores digits for # of digits in int
+            temp /= 10;
+
+           
+        } //end while loop, temp != 0
+
+        if (exists == true) //for all primes without zeroes and identical digits
+        {
+            //cout << i << endl;
+            //cout << primes[i] << " ";
+            for (int j = 1; j < vect.size() + 1; j++)
+            {
+                if (find(begin(digits), end(digits), j) != end(digits) == true)
+                {
+                    exists = false;
+                    break;
+                }
+            }
+        }
+        if (exists == true)
+            cout << primes[i] << " "; //prints all viable pandigital primes
+        //max found is 7652413
+    } // end for loop, for each prime
+
+
+
+
+}
